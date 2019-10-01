5bfcafe71bce6c13bd026df6f20678ec11a10b29
gauravkm12345 <56034907+gauravkm12345@users.noreply.github.com>
2019-10-01 21:50:03 +0530

    Update Square Pattern.py

--- Square Pattern.py
+++ Square Pattern.py
@@ -1,6 +1,6 @@
-n=int(input("Enter no. of rows: "))
-for i in range(n):
-    for j in range(0,i)"
+r=int(input("Enter the no. of rows: "))
+for i in range(r):
+    for j in range(0,i):
         print(j,end=" ")
     print('\n')
 
